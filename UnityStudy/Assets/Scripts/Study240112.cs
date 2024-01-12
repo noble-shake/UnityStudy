1b0b4a6db3abe5652d226d282f73a4ee67d44e01
noble-shake <coppgabliw@gmail.com>
2024-01-12 21:21:02 +0900

    Update Study240112.cs

--- UnityStudy/Assets/Scripts/Study240112.cs
+++ UnityStudy/Assets/Scripts/Study240112.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class Study240112 : MonoBehaviour
 {
+    /*
+     * 2024-01-12 unity class
+     * [LEARNED] local variable and global variable description.
+     * [LEARNED] previous and after value change operator c++ /  ++c ...
+     * [LEARNED] if ~ else if, condtion.  >, <, >=, <=, ==, !
+     */
+
     int global_int = 10;
     // Start is called before the first frame update
     void Start()
@@ -18,11 +25,27 @@ public class Study240112 : MonoBehaviour
         int c = b++;
         int d = a * ++b;
         Debug.Log(d);
+        a--;
+        Debug.Log(a);
 
-     }
+        def_test();
+        condition_test(true);
+        condition_test(false);
+    }
 
     private void def_test()
     {
         global_int++;
+    }
+
+    private void condition_test(bool condtion) {
+        if (condtion)
+        {
+            Debug.Log($"condition is {condtion}");
+        }
+        else if (!condtion)
+        {
+            Debug.Log($"condition is {condtion}");
+        }
     }
 }
