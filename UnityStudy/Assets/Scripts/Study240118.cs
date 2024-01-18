b44d29ec05bc2d2ac75687d53f697461a3667b3b
noble-shake <154754400+noble-shake@users.noreply.github.com>
2024-01-18 20:00:59 +0900

    2024-01-18 Unity Class

---
+++ UnityStudy/Assets/Scripts/Study240118.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Study240118 : MonoBehaviour
+{
+    /*
+     * 2024-01-18 unity class
+     * [PREVIOUS] remind for and while loop
+     * [PROBLEM 1] make a star Pyramid using for
+     * [PROBLEM 2] make a star Reverse Pyramid using for
+     * [PROBLEM 3] make a star Pyramid With Blanks using for
+     * [PROBLEM] make a star. using for
+     * [LEARNED] double loop.
+     */
+
+    private void Start()
+    {
+
+    }
+
+}
