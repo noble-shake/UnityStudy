494a09f7bd10c094ac10bcd01697646e4a6437b6
noble-shake <154754400+noble-shake@users.noreply.github.com>
2024-01-18 21:09:21 +0900

    2024-01-18 UnityClass
         * 2024-01-18 unity class
         * [PREVIOUS] remind PROBLEM 1 ~ 4 using "for". then use "while"
         * [LEARNED] Function.
         * [LEARNED] Unity Life Cycle.  https://docs.unity3d.com/Manual/ExecutionOrder.html
         * [LEARNED] Awake - Start - FixedUpdate - Update - LateUpdate
         * [LEARNED] return - if only return or zero(0) return, exit function. or not, return pre-defining variable.

--- UnityStudy/Assets/Scripts/Study240117.cs
+++ UnityStudy/Assets/Scripts/Study240117.cs
@@ -1,11 +1,14 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class Study240117 : MonoBehaviour
 {
+    public string star = "』";
+    public string blank = "　";
     /*
      * 2024-01-17 unity class
      * [PREVIOUS] remind for and while loop
@@ -18,12 +21,15 @@ public class Study240117 : MonoBehaviour
 
     private void Start()
     {
+
+    }
+
+    public void problem_1_function() {
         // [PROBLEM 1] make a star Pyramid. using for
         // Condition : blank "  ", Star "』"
         Debug.Log("[PROBLEM 1] make a star Pyramid. using for");
-        string star = "』";
-        string blank = "　";
         
+
         string s1Value = "";
         string total1Value = "";
         for (int iNum = 0; iNum < 5; iNum++)
@@ -33,6 +39,8 @@ public class Study240117 : MonoBehaviour
         }
         Debug.Log(total1Value);
 
+    }
+    public void problem_2_function() {
         // [PROBLEM 2] make a star Reverse Pyramid with blank. using for
         Debug.Log("[PROBLEM 2] make a star Reverse Pyramid. using for");
 
@@ -40,28 +48,33 @@ public class Study240117 : MonoBehaviour
         for (int iNum = 5; iNum > 0; iNum--)
         {
             string s2Value = "";
-            for (int iter_num = 0; iter_num < iNum; iter_num++) {
+            for (int iter_num = 0; iter_num < iNum; iter_num++)
+            {
                 s2Value += star;
             }
             total2Value += s2Value + "\n";
         }
         Debug.Log(total2Value);
-
+    }
+    public void problem_3_function() {
         // [PROBLEM 3] make a star Pyramid with blank. using for
         Debug.Log("[PROBLEM 3] make a star Pyramid with blank. using for");
 
         string s3Value = "";
         string total3Value = "";
-        for (int iNum = 0; iNum < 5; iNum++) {
+        for (int iNum = 0; iNum < 5; iNum++)
+        {
             string bValue = "";
-            for (int ibNum = 5 - iNum -1; ibNum > 0; ibNum--) {
+            for (int ibNum = 5 - iNum - 1; ibNum > 0; ibNum--)
+            {
                 bValue += blank;
             }
             s3Value += star;
             total3Value += bValue + s3Value + "\n";
         }
         Debug.Log(total3Value);
-
+    }
+    public void problem_4_function() {
         // [PROBLEM 4] make a star Reverse Pyramid with blank. using for
         Debug.Log("[PROBLEM 4] make a star Pyramid with blank. using for");
 
@@ -81,7 +94,6 @@ public class Study240117 : MonoBehaviour
             total4Value += bValue + s4Value + "\n";
         }
         Debug.Log(total4Value);
-
 
     }
 
--- UnityStudy/Assets/Scripts/Study240118.cs
+++ UnityStudy/Assets/Scripts/Study240118.cs
@@ -6,19 +6,62 @@ using UnityEngine;
 
 public class Study240118 : MonoBehaviour
 {
+    public Study240117 previous_study;
     /*
      * 2024-01-18 unity class
-     * [PREVIOUS] remind for and while loop
-     * [PROBLEM 1] make a star Pyramid using for
-     * [PROBLEM 2] make a star Reverse Pyramid using for
-     * [PROBLEM 3] make a star Pyramid With Blanks using for
-     * [PROBLEM] make a star. using for
-     * [LEARNED] double loop.
+     * [PREVIOUS] remind PROBLEM 1 ~ 4 using "for". then use "while"
+     * [LEARNED] Function.
+     * [LEARNED] Unity Life Cycle.  https://docs.unity3d.com/Manual/ExecutionOrder.html
+     * [LEARNED] Awake - Start - FixedUpdate - Update - LateUpdate 
+     * [LEARNED] return - if only return or zero(0) return, exit function. or not, return pre-defining variable.
+     * 
      */
+
+    private void Awake()
+    {
+        // First Called. Only once run.
+        // normally used define itself
+        // all of Awake function order is random. setting from Project Setting "Script Execution Order"
+        Debug.Log("Awake");
+        
+    }
 
     private void Start()
     {
-
+        // Second Called. Only once run.
+        // get reference from other scripts.
+        Debug.Log("Start");
+        previous_study = GetComponent<Study240117>();
+        previous_study.problem_1_function();
+        previous_study.problem_2_function();
+        previous_study.problem_3_function();
+        previous_study.problem_4_function();
     }
 
+    private void FixedUpdate()
+    {
+        // not related frames. has specific timing.(0.02s) recommended use Physics implement
+        Debug.Log("FixedUpdate");
+    }
+    private void Update()
+    {
+        // each framed run. algoritm works
+        Debug.Log("Update");
+    }
+
+    private void LateUpdate()
+    {
+        // same with update but, need to check at alst. Camera Moving
+        Debug.Log("LateUpdate");
+    }
+
+    private void function1()
+    {
+        
+    }
+
+    private void function2() { }
+    private void function3() { }
 }
+
+
