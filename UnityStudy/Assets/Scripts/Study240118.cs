8f32fcdb867be01adf09aa2352cd0ac769a4a963
noble-shake <154754400+noble-shake@users.noreply.github.com>
2024-01-18 21:59:48 +0900

    2024-01-18 UnityClass
    2024-01-18 unity class
    [PREVIOUS] remind [make star pyramid] using "for". then use "while"
    [LEARNED] Function.
    [LEARNED] Unity Life Cycle.  https://docs.unity3d.com/Manual/ExecutionOrder.html
    [LEARNED] Awake - Start - FixedUpdate - Update - LateUpdate
    [LEARNED] return - if only return or zero(0) return, exit function. or not, return pre-defining variable.
    [LEARNED] overloading ~ same function name, classify different input / output cases
    [PROBLEM 1] Plus Function
    [PROBLEM 2] Minus Function
    [PROBLEM 3] Multiply Function
    [PROBLEM 4] Divide Function
    [LEARNED] rich text for visibility https://docs.unity3d.com/Packages/com.unity.ugui@1.0/manual/StyledText.html

--- UnityStudy/Assets/Scripts/Study240118.cs
+++ UnityStudy/Assets/Scripts/Study240118.cs
@@ -6,16 +6,23 @@ using UnityEngine;
 
 public class Study240118 : MonoBehaviour
 {
-    public Study240117 previous_study;
     /*
      * 2024-01-18 unity class
-     * [PREVIOUS] remind PROBLEM 1 ~ 4 using "for". then use "while"
+     * [PREVIOUS] remind [make star pyramid] using "for". then use "while"
      * [LEARNED] Function.
      * [LEARNED] Unity Life Cycle.  https://docs.unity3d.com/Manual/ExecutionOrder.html
      * [LEARNED] Awake - Start - FixedUpdate - Update - LateUpdate 
      * [LEARNED] return - if only return or zero(0) return, exit function. or not, return pre-defining variable.
-     * 
+     * [LEARNED] overloading ~ same function name, classify different input / output cases
+     * [PROBLEM 1] Plus Function 
+     * [PROBLEM 2] Minus Function 
+     * [PROBLEM 3] Multiply Function 
+     * [PROBLEM 4] Divide Function 
+     * [LEARNED] rich text for visibility https://docs.unity3d.com/Packages/com.unity.ugui@1.0/manual/StyledText.html
      */
+
+    // import other script
+    public Study240117 previous_study;
 
     private void Awake()
     {
@@ -36,6 +43,24 @@ public class Study240118 : MonoBehaviour
         previous_study.problem_2_function();
         previous_study.problem_3_function();
         previous_study.problem_4_function();
+
+        ExamFunction();
+        ExamFunction(1);
+        ExamFunction(0.1f);
+        ExamFunction("null");
+        ExamFunction(1, "null", "haha", "heh heh");
+        Debug.Log(ExamFunction(1, 2, 3));
+        
+        //[PROBLEM 1] Plus Function
+        //[PROBLEM 2] Minus Function 
+        //[PROBLEM 3] Multiply Function 
+        //[PROBLEM 4] Divide Function 
+        PlusFunction(3, 1);
+        MinusFunction(3, 1);
+        MultiplyFunction(3, 3);
+        DivideFunction(3, 2);
+
+        Debug.Log($"<color=red> text example. </color>");
     }
 
     private void FixedUpdate()
@@ -55,13 +80,45 @@ public class Study240118 : MonoBehaviour
         Debug.Log("LateUpdate");
     }
 
-    private void function1()
-    {
-        
+    private void ExamFunction() { }
+    private int ExamFunction(int var) { Debug.Log(var); return var; }
+    private float ExamFunction(float var) { Debug.Log(var); return var; }
+    private string ExamFunction(string var) { Debug.Log(var); return var; }
+    private string ExamFunction(params int[] var) // undefined parameters
+    { 
+        int count = var.Length;
+        string data = "";
+        foreach (int v in var)
+        {data += $"Function Exam {v}\n";}
+        return data; 
     }
 
-    private void function2() { }
-    private void function3() { }
+    private void ExamFunction(int var1, params string[] var2) // undefined parameters
+    {
+        int count = var2.Length;
+        Debug.Log($"Function Exam {var1}");
+        for (int i = 0; i < count; i++)
+        {
+            Debug.Log($"Function Exam {var2[i]}");
+        }
+        return;
+    }
+
+    private int PlusFunction(int var1, int var2) {
+        return var1 + var2;
+    }
+    private int MinusFunction(int var1, int var2)
+    {
+        return var1 - var2;
+    }
+    private int MultiplyFunction(int var1, int var2)
+    {
+        return var1 * var2;
+    }
+    private int DivideFunction(int var1, int var2)
+    {
+        return var1 / var2;
+    }
 }
 
 
