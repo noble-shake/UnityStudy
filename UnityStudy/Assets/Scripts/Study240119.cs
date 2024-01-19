5351857ca5fde2949ea761000a3430bf34a95a81
noble-shake <154754400+noble-shake@users.noreply.github.com>
2024-01-19 20:13:22 +0900

    2024-01-19 UnityClass

---
+++ UnityStudy/Assets/Scripts/Study240119.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Study240119 : MonoBehaviour
+{
+    /*
+     * 2024-01-19 unity class
+     * [PREVIOUS] remind [make star pyramid] using "for". then use "while"
+     * [LEARNED] Function.
+     * [PROBLEM 1] Plus Function 
+     */
+
+    private void Start()
+    {
+
+    }
+}
+
+
