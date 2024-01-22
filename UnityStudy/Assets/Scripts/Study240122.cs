3b65f6a6dba22618b6502968508e15da0ac49f1f
noble-shake <154754400+noble-shake@users.noreply.github.com>
2024-01-22 20:04:04 +0900

    2024-01-22 Unity Class

---
+++ UnityStudy/Assets/Scripts/Study240122.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+public class Study240122 : MonoBehaviour
+{
+    /*
+     * 2024-01-22 unity class
+     * [PREVIOUS] remind Function.
+     * [LEARNED] 
+     * [PROBLEM 1]
+     */
+
+    private void Start()
+    {
+       
+    }
+}
