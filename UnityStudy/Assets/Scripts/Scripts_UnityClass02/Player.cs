fc9db9b6b30df8233a2fdc61a0805e9f05eb046f
HShake <154754400+noble-shake@users.noreply.github.com>
2024-02-13 21:54:24 +0900

    2024-02-13 UnityClass
    2024-02-13 UnityClass
    Animation
    Moving Function
    Moving Limit

--- UnityStudy/Assets/Scripts/Scripts_UnityClass02/Player.cs
+++ UnityStudy/Assets/Scripts/Scripts_UnityClass02/Player.cs
@@ -10,26 +10,81 @@ public class Player : MonoBehaviour
     // [LEARNED] Input Methods
     // [LEARNED] Movemant Player
     [SerializeField] float speed;
+    [SerializeField] Camera cam;
 
+    [SerializeField] Vector2 minScreen;
+    [SerializeField] Vector2 maxScreen;
+
+    Animator anim;
+    Vector2 moveDir;
 
     void Start()
     {
-        
+       anim = GetComponent<Animator>();
     }
 
     void Update()
     {
         moving();   // Ctrl + '.' -> automatically Definition Generate.     
+        checkMovePostion();
+        animating();
     }
 
     private void moving()
     {
-        float vertical = Input.GetAxisRaw("Vertical");
-        float horizontal = Input.GetAxisRaw("Horizontal");
+        moveDir.y = Input.GetAxisRaw("Vertical");
+        moveDir.x = Input.GetAxisRaw("Horizontal"); // -1 ~ 0 ~ 1 이므로 animation에서 condition params 0 less or greather 를 해주면 애니메이션 컨디션 분기 가능.
         // Debug.Log($"{vertical} / {horizontal}");
-        transform.position += new Vector3(horizontal, vertical, transform.position.z) * Time.deltaTime * speed;
+        transform.position += new Vector3(moveDir.x, moveDir.y, transform.position.z) * Time.deltaTime * speed;
+
+        // 2024-02-13 Unity LifeCycle ~ Game Logic Section and Physics Section Collision event exist.
+
     }
 
+    /// <summary>
+    /// Limit Player's Moving Range.
+    /// </summary>
+    private void checkMovePostion() {
+        // Camera View Port (cam based vector),  World Space (absolute vector)
+        Vector3 curPos = cam.WorldToViewportPoint(transform.position);
+        if (curPos.x < minScreen.x)
+        {
+            curPos.x = minScreen.x;
+        }
+        else if (curPos.x > maxScreen.x) {
+            curPos.x = maxScreen.x;
+        }
+
+        if (curPos.y < minScreen.y)
+        {
+            curPos.y = minScreen.y;
+        }
+        else if (curPos.y > maxScreen.y)
+        {
+            curPos.y = maxScreen.y;
+        }
+
+        Vector3 fixedPos = cam.ViewportToWorldPoint(curPos);
+        transform.position = fixedPos;
+    }
+
+    /// <summary>
+    /// 
+    /// List Append Condition Parameters
+    /// Int, Bool, float, trigger.
+    /// float, int -> compare condition
+    /// bool -> 2 transition
+    /// Trigger ...? 
+    /// 
+    /// </summary>
+    private void animating() {
+        // anim.SetInteger(0, (int)moveDir.x);
+        anim.SetInteger("Horizontal", (int)moveDir.x);
+    }
+
+    /// <summary>
+    /// Not Use Function.
+    /// </summary>
     private void movingDescription() {
         // Mouse,  0 : left click, 1 right click, 2 wheel click
         Input.GetMouseButton(0); // 마우스를 꾹 누르고 있는 것을 체크
